76f2b1d7d63298ae05a2d12847212dc04fc7911a
admin <admin@DESKTOP-8L2IG92>
2021-06-01 06:24:23 +0900

    ex4no23

---
+++ ex4no23/src/ex4no23/ex4no23.java
@@ -0,0 +1,12 @@
+package ex4no23;
+
+public class ex4no23 {  //  168p
+	public static void main(String[] args) {
+		int i = 5 ;
+		
+		while( i-- != 0 ) {
+			System.out.println( i + " - I can do it.");
+		}
+	}
+
+}
